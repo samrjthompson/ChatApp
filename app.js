300c5113a986b533294e197df91c58ae012f1372
Sam Thompson <66477261+samrjthompson@users.noreply.github.com>
2022-04-07 14:35:52 +0100

    Setup server

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+
+const port = 3000
+
+// CONNECTION SETUP
+mongoose.connect('mongodb://localhost:27017/chat-app')
+.then(() => {
+    console.log('MONGO CONNECTION OPEN!')
+})
+.catch(err => {
+    console.log('ERROR: MONGO Connection Failed!')
+    console.log(err)
+})
+/* ################################################# */
+
+// MIDDLEWARE
+app.use(express.urlencoded({extended: true})) // ensures urlencoded data can be parsed by express when we call req.body
+//app.use(express.json())
+app.use(methodOverride('_method'))
+/* ################################################# */
+
+// these two ensure the app.get or app.post code refers to rootdirectory/views folder
+// e.g. res.render('campgrounds/show') refers to ~/views/campgrounds/show.ejs
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
+})
