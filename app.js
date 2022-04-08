e49b77cc7860419506ec809407ab2aea47cbb172
Sam Thompson <66477261+samrjthompson@users.noreply.github.com>
2022-04-08 16:20:40 +0100

    added ejs-mate boilerplate basics

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const User = require('./models/user')
 const morgan = require('morgan')
+const ejsMate = require('ejs-mate')
+
+// MODELS
+const User = require('./models/user')
 const MessageBoard = require('./models/messageBoard')
+/* ################################################# */
 
 const port = 3000
 
@@ -21,6 +25,7 @@ mongoose.connect('mongodb://localhost:27017/chat-app')
 /* ################################################# */
 
 // MIDDLEWARE
+app.engine('ejs', ejsMate)
 app.use(express.urlencoded({extended: true})) // ensures urlencoded data can be parsed by express when we call req.body
 //app.use(express.json())
 app.use(methodOverride('_method'))
